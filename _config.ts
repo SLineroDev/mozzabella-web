f494a9a05692122fe6aed287839dc20902ee4632
Sergio Linero <slinerodev@gmail.com>
2024-03-15 14:43:08 +0100

    chore: updated packages

--- _config.ts
+++ _config.ts
@@ -1,5 +1,5 @@
 const DOMAIN = 'localhost';
-const BUSINESS_LINK = '';
+const BUSINESS_LINK = 'localhost';
 const GITHUB_REPOSITORY_LINK = 'https://github.com/slinerodev/astro-starter-template';
 
 export { BUSINESS_LINK, DOMAIN, GITHUB_REPOSITORY_LINK };
