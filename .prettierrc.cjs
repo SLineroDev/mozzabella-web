dbe5820396afbf2432a5a6a4cfdb6f21579e5605
Sergio Linero <slinerodev@gmail.com>
2024-03-15 13:44:46 +0100

    First version of astro template

---
+++ .prettierrc.cjs
@@ -0,0 +1,28 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: 'always',
+  endOfLine: 'crlf',
+  plugins: [
+    'prettier-plugin-astro',
+    '@ianvs/prettier-plugin-sort-imports',
+    'prettier-plugin-tailwindcss' // must be last
+  ],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
+      }
+    }
+  ],
+  importOrder: ['<TYPES>', '<TYPES>^[.]', '', '<THIRD_PARTY_MODULES>', '', '^[@]/', '', '^[.]']
+};
