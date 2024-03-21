130fd7320b9858c8423510b5ba2b04816f98e865
Sergio Linero <slinerodev@gmail.com>
2024-03-21 12:16:35 +0100

    Update HamburgerButton and Nav components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,12 @@ export default {
   theme: {
     container: {
       center: true,
-      padding: '3rem'
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem'
+      }
     },
     extend: {
       colors: {
